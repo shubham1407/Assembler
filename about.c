133e2ce1e44702713ad027c863c37894799b8dd4
Ashutosh Verma <ashutoshvrm8@gmail.com>
2015-11-02 15:32:48 +0530

    Create about.c

---
+++ about.c
@@ -0,0 +1,13 @@
+# Assembler
+Project
+/*  ===========================================================================================
+    Program  : Test
+    Authors  : Ashutosh Verma( B.tech - CSE, 5th Semester(A), GEU)
+               Shubham Gupta ( B.tech - CSE, 5th Semester(C), GEU)
+    Language : C
+    Compiler : GNU GCC Compiler for MSP430
+    IDE      : Codeblocks svn 8549
+    Email-id : ashutoshvrm8@gmail.com
+               sgshubhamgupta77@gmail.com
+    ===========================================================================================
+*/
