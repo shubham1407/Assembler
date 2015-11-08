faad0d3145c6653d36dff60494a13e6a23f7997a
Ashutosh Verma <ashutoshvrm8@gmail.com>
2015-11-08 13:02:34 +0530

    Update Addressing.c

--- Addressing.c
+++ Addressing.c
@@ -1,13 +1,3 @@
-/*  ===========================================================================================
-    Program  :
-    Author   : Ashutosh Verma( B.tech - CSE, 5th Semester(A), GEU)
-    Language : C
-    Compiler : GNU GCC Compiler for MSP430
-    IDE      : Codeblocks svn 8549
-    Email-id : ashutoshvrm8@gmail.com
-    ===========================================================================================
-*/
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -17,7 +7,7 @@ FILE *fpr=NULL,*fpw=NULL;
 int main()
 {
     char label[10],mnemonic[10],operand[10],program_name[10];
-    int address=0,first_address=0,previous_address=0,last_address=0,line=0;
+    int address=0,first_address=0,previous_address=0,last_address=0,line=0,c=0,p1=0,p2=0,i=0,length,x=0;
 
     /*Adressing*/
 
@@ -27,8 +17,6 @@ int main()
     fscanf(fpr,"%s%s%d",&program_name,&mnemonic,&address);
     line++;
     fprintf(fpw,"%d\t\t%s\t%s\t%d\n",line,program_name,mnemonic,address);
-
-    printf("\n%d\n",address);
 
     while(strcmp(mnemonic,"END")!=0)
     {
@@ -40,7 +28,32 @@ int main()
             if(line>=2)
             {
                 fprintf(fpw,"%d\t%.4d\t%s\t%s\t%s\n",line,address,label,mnemonic,operand);
-                address+=3;
+
+                if(strcmp(mnemonic,"BYTE")==0)
+                {
+                    length=0;
+                    length=strlen(operand);
+
+                    length-=3; /*excluding C'' */
+
+                    address+=length;
+                }
+                else if(strcmp(mnemonic,"WORD")==0)
+                {
+                    address+=3;
+                }
+                else if(strcmp(mnemonic,"RESW")==0)
+                {
+                    x=atoi(operand); /*converting character into integer */
+                    address+=(x*3); /*1 WORD = 3 BYTES */
+                }
+                else if(strcmp(mnemonic,"RESB")==0)
+                {
+                    x=atoi(operand); /*change RESB's mnemonic into hexadecimal --> x, address+=x; */
+                    address+=x;
+                }
+                else
+                    address+=3;
             }
         }
         else
