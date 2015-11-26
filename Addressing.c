c9324f95db4e93ea6ad721fd873a549ef5ce4aa1
Shubham Gupta <sgshubhamgupta77@gmail.com>
2015-11-26 10:05:56 +0530

    Update Addressing.c

--- Addressing.c
+++ Addressing.c
@@ -6,8 +6,8 @@ FILE *fpr=NULL,*fpw=NULL,*fp1=NULL,*fp2=NULL;
 
 int main()
 {
-    char label[10],mnemonic[10] ,operand[10],program_name[10],sym_label[10][10],sym_address[10][10];
-    int address=0,line=0,length,x=0,first_address=0,last_address=0,object_code=0,i=0,j=0,k=0;
+    char label[10],mnemonic[10] ,operand[10],program_name[10],sym_label[20][10],sym_address[20][10],opcode[10];
+    int address=0,line=0,length,x=0,first_address=0,last_address=0,i=0,j=0,k=0,pos1=0,pos2=0,flag1=0,flag2=0,p1=0,p2=0,l=0,z=0;
 
     char op_mnemonic[26][10]={"LDAC","STAC","SUBJ","MULT","STRL","DIVD","ADDA","STRCH","DT",
     "JMPEQ","LOADCH","DW","SUBR","FIXR","FIX","JMPLT","COMP","CLR","LOADB","J","DR","COMPR","STREX","LOADT","LOADL","LOADX"};
@@ -96,6 +96,7 @@ int main()
         {
             fprintf(fpw,"%s  %X\n",label,address);
             j++;
+            z=j;
         }
     }
 
@@ -107,6 +108,7 @@ int main()
     /*Extracting data from symtab*/
 
     fp1=fopen("symtab.txt","r");
+    i=0;
 
     while(j--)
     {
@@ -115,6 +117,85 @@ int main()
     }
 
     fclose(fp1);
+
+    /*Generating opcodes*/
+
+    fpr=fopen("addressed_file.txt","r");
+    fpw=fopen("intermediate.txt","w");
+
+    fscanf(fpr,"%d%s%s%s",&line,&program_name,&mnemonic,&operand);
+    fprintf(fpw,"%d\t\t%s\t%s\t%s\t\t--\n",line,program_name,mnemonic,operand);
+
+    while(strcmp(mnemonic,"END")!=0)
+    {
+        fscanf(fpr,"%d%X%s%s%s",&line,&address,&label,&mnemonic,&operand);
+
+        if(strcmp(mnemonic,"WORD")==0)
+            fprintf(fpw,"%d\t%.4X\t%s\t%s\t%s\t\t%.6X\n",line,address,label,mnemonic,operand,atoi(operand));
+
+        else if(strcmp(mnemonic,"BYTE")==0)
+        {
+            l=strlen(operand);
+            k=0;
+
+            for(i=2;i<l-1;i++)
+                opcode[k++]=operand[i];
+            opcode[k]='\0';
+
+            if(operand[0]=='C')
+                fprintf(fpw,"%d\t%.4X\t%s\t%s\t%s\t\t454F46\n",line,address,label,mnemonic,operand);
+
+            if(operand[0]=='X')
+                fprintf(fpw,"%d\t%.4X\t%s\t%s\t%s\t\t%s\n",line,address,label,mnemonic,operand,opcode);
+        }
+
+        else if(strcmp(mnemonic,"RESERW")==0 || strcmp(mnemonic,"RESERB")==0 || strcmp(mnemonic,"START")==0 || strcmp(mnemonic,"END")==0)
+            fprintf(fpw,"%d\t%.4X\t%s\t%s\t%s\t\t--\n",line,address,label,mnemonic,operand);
+
+        else if(strcmp(mnemonic,"SUBR")==0)
+            fprintf(fpw,"%d\t%.4X\t%s\t%s\t%s\t\t4C0000\n",line,address,label,mnemonic,operand);
+
+        else
+        {
+            pos1=pos2=0;
+
+            for(i=0;i<26;i++)
+            {
+                if(strcmp(mnemonic,op_mnemonic[i])==0)
+                {
+                    pos1=i;
+                    break;
+                }
+            }
+
+            for(j=0;j<z;j++)
+            {
+                if(strcmp(operand,sym_label[j])==0)
+                {
+                    pos2=j;
+                    break;
+                }
+                if(strcmp(sym_label[j],"BUFFER")==0)
+                    z=j;
+
+                if(strcmp(operand,"BUFFER,X")==0)
+                {
+                    pos2=z;
+                    break;
+                }
+            }
+
+            if(strcmp(operand,"BUFFER,X")==0)
+            {
+                fprintf(fpw,"%d\t%.4X\t%s\t%s\t%s\t%s%s\n",line,address,label,mnemonic,operand,op_opcode[pos1],sym_address[pos2]);
+            }
+            else
+                fprintf(fpw,"%d\t%.4X\t%s\t%s\t%s\t\t%s%s\n",line,address,label,mnemonic,operand,op_opcode[pos1],sym_address[pos2]);
+        }
+    }
+
+    fclose(fpr);
+    fclose(fpw);
 
     return 0;
 }
